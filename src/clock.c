2aa6d6f11439ed67beb2aa79b22662e7e16607c0
Hugo Lizarraga <lizarragahugo@gmail.com>
2023-01-28 20:15:28 -0300

    Version_05 TPN5 Tercer prueba

--- src/clock.c
+++ src/clock.c
@@ -3,9 +3,12 @@
 
 #define TIME_SIZE 6
 #define START_VALUE 0
+#define SECONDS_UNITS 5
 
 struct clock_s{
     bool valid;
+    uint16_t ticks_per_second;
+    uint16_t ticks_count;
     uint8_t time[TIME_SIZE];
 };
 
@@ -13,6 +16,8 @@ static struct clock_s instances;
 
 clock_t ClockCreate(uint16_t ticks_per_second){
     instances.valid = false;
+    instances.ticks_count = START_VALUE;  
+    instances.ticks_per_second = ticks_per_second = ticks_per_second;
     memset(instances.time, START_VALUE, TIME_SIZE);
     return &instances;
 }
@@ -25,4 +30,12 @@ bool ClockGetTime(clock_t clock, uint8_t *time, uint8_t size){
 void ClockSetupTime(clock_t clock, uint8_t const * const time, uint8_t size){
     memcpy(clock->time, time, size);
     clock->valid = true;
+}
+
+void ClockNewTick(clock_t clock){
+    clock->ticks_count++;
+    if(clock->ticks_count == clock->ticks_per_second){
+        clock->ticks_count = START_VALUE;
+        clock->time[SECONDS_UNITS]++;
+    }
 }
--- test/test_reloj.c
+++ test/test_reloj.c
@@ -2,10 +2,10 @@
 #include "unity.h"
 
 /*
-1) Configurar la libreria, consultar la hora y tiene que ser invalida.
-2) Configurar la libraria, ajustar la hora (con valores correctos), consultar la hora y tiene que ser valida.
+x-> 1) Configurar la libreria, consultar la hora y tiene que ser invalida.
+x-> 2) Configurar la libraria, ajustar la hora (con valores correctos), consultar la hora y tiene que ser valida.
 3) Configurar la libraria, ajustar la hora (con valores incorrectos) tiene devolver un error y consultar la hora y tiene que ser invalida.
-4) Simular el paso de n ciclos de reloj, consultar la hora y verificar que avano un segunto.
+x-> 4) Simular el paso de n ciclos de reloj, consultar la hora y verificar que avano un segunto.
 5) Simular el paso de 10*n ciclos, consultar la hora y verificar que avanzo diez segundos
 ..... Y varias igual
 6) Configurar la hora de la alarma (con valores correctos) y revisar si la guarda.
@@ -39,5 +39,21 @@ void test_set_up_current_time(void){
     clock_t reloj = ClockCreate(TICKS_PER_SECOND);
     ClockSetupTime(reloj, INICIAL, sizeof(INICIAL));  
     TEST_ASSERT_TRUE(ClockGetTime(reloj, hora, sizeof(hora)));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, sizeof(ESPERADO));
+}
+
+// Simular el paso de n ciclos de reloj, consultar la hora y verificar que avano un segunto.
+
+void test_one_second_elapsed(void){
+    static const uint8_t INICIAL[] = {1, 2, 3, 4};
+    static const uint8_t ESPERADO[] = {1, 2, 3, 4, 0, 1};
+    uint8_t hora[6];
+
+    clock_t reloj = ClockCreate(TICKS_PER_SECOND);
+    ClockSetupTime(reloj, INICIAL, sizeof(INICIAL));
+    for(int index = 0; index < TICKS_PER_SECOND; index++){
+        ClockNewTick(reloj);
+    }
+    ClockGetTime(reloj, hora, sizeof(hora));
     TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, sizeof(ESPERADO));
 }
